6315c41b7bb2e50c5fb93040b91a15d18a23b34e
Bohdan 👾 <Bohd4n@proton.me>
2024-09-05 23:58:32 +0300

    Delete api directory

--- api/coinbase.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def get_price(coin):
-    url = f"https://api.coinbase.com/v2/prices/{coin.upper()}-USD/spot"
-    response = requests.get(url)
-    data = response.json()
-    return float(data['data']['amount'])
