7a414f9e2eec2d959aa15449bce17000f80f1123
Bohdan 👾 <Bohd4n@proton.me>
2024-09-15 00:04:47 +0300

    Delete crypto_tracker directory

--- crypto_tracker/api/gateio.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def get_price(coin):
-    url = f"https://api.gate.io/api2/1/ticker/{coin.lower()}_usdt"
-    response = requests.get(url)
-    data = response.json()
-    return float(data['last'])
--- crypto_tracker/api/kraken.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def get_price(coin):
-    url = f"https://api.kraken.com/0/public/Ticker?pair={coin.upper()}USD"
-    response = requests.get(url)
-    data = response.json()
-    return float(data['result'][list(data['result'].keys())[0]]['c'][0])
